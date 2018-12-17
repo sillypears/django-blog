f8d3f2646e94dc3214de6f8594500aea03d7a1d6
sillypears <sillypears@users.noreply.github.com>
2018-12-17 16:35:41 -0500

    End of chapter 6

--- blog/tests.py
+++ blog/tests.py
@@ -43,3 +43,28 @@ class BlogTests(TestCase):
         self.assertEqual(no_resp.status_code, 404)
         self.assertContains(resp, 'Good one')
         self.assertTemplateUsed(resp, 'post_detail.html')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1')
+
+    def test_post_create_view(self):
+        resp = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,
+        })
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'New title')
+        self.assertContains(resp, 'New text')
+
+    def test_post_update_view(self):  # new
+        resp = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+        self.assertEqual(resp.status_code, 302)
+
+    def test_post_delete_view(self):  # new
+        response = self.client.get(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import ListView, DetailView
-from . models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
 
 class BlogListView(ListView):
@@ -16,3 +18,18 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = 'blog_post'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
