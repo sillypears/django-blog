318be7c75b77dc26d0d43478a307819a3ad72036
sillypears <sillypears@users.noreply.github.com>
2018-12-16 22:57:29 -0500

    Works!

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
-
+from django.test import client, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your tests here.
+
+from .models import Post
+
+
+class BlogTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@test.test',
+            password='secretpasswordhehe',
+        )
+
+        self.post = Post.objects.create(
+            title='Good one',
+            body='Body content',
+            author=self.user,
+        )
+
+    def test_string_representation(self):
+        post = Post(title='A test title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'Good one')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Body content')
+
+    def test_post_list_view(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertContains(resp, 'Body content')
+        self.assertTemplateUsed(resp, 'home.html')
+
+    def test_post_detail_view(self):
+        resp = self.client.get('/post/1')
+        no_resp = self.client.get('/post/10000')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(no_resp.status_code, 404)
+        self.assertContains(resp, 'Good one')
+        self.assertTemplateUsed(resp, 'post_detail.html')
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . models import Post
 
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    context_object_name = 'all_posts_list'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'blog_post'
